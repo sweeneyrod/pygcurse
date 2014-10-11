930dc66aee2ab7b434820461ce8a6a57d47f443b
Roddy MacSween <roddylm2@gmail.com>
2014-10-11 18:46:46 +0100

    Removed unnecessary code (mostly for testing)

--- pygcurse/__init__.py
+++ pygcurse/__init__.py
@@ -12,9 +12,6 @@ Released under a Simplified BSD License
 
 __version__ = '0.10.0'
 
-import traceback
-
-
 import time
 import datetime
 import sys
@@ -162,7 +159,6 @@ class PygcurseSurface(object):
         # inputcursorblinking is a boolean variable that tracks if the input cursor should be blinking or stay solid.
         self._inputcursormode = None # either None, 'underline', 'insert' or 'box'
         self.inputcursorblinking = inputcursorblinking
-        print(self.inputcursorblinking)
         self.inputcursorblinkspeed = inputcursorblinkspeed
         self._inputcursorx = 0
         self._inputcursory = 0
@@ -339,7 +335,7 @@ def pygprint(self, obj='', *objs, sep=' ', end='\n', fgcolor=None, bgcolor=None,
         return spaces % self._width, y + int(spaces / self._width)
 
 
-    def update(self, draw_cursor=True):
+    def update(self):
         """
         Update the encapsulated pygame.Surface object to match the state of this PygcurseSurface object. This needs to be done before the pygame.Surface object is blitted to the screen if you want the most up-to-date state displayed.
 
@@ -388,8 +384,7 @@ def pygprint(self, obj='', *objs, sep=' ', end='\n', fgcolor=None, bgcolor=None,
                     charrect.bottom = self._cellheight * (y + 1) # TODO - not correct, this would put stuff like g, p, q higher than normal.
                     self._surfaceobj.blit(charsurf, charrect)
         
-        if draw_cursor:
-            self._drawinputcursor()
+        self._drawinputcursor()
 
         # automatically blit to "window surface" pygame.Surface object if it was set.
         if self._windowsurface is not None and self._autoblit:
@@ -404,12 +399,7 @@ def pygprint(self, obj='', *objs, sep=' ', end='\n', fgcolor=None, bgcolor=None,
         if self._inputcursormode is not None and self._inputcursorx is not None and self._inputcursory is not None:
             x = self._inputcursorx # syntactic sugar
             y = self._inputcursory # syntactic sugar
-            
-            with open("f.txt", "a") as f:
-                traceback.print_stack(limit=10, file=f)
-                f.write(str(x))
-                f.write("\n")
-                
+
             current_time = datetime.datetime.now()
             microseconds = current_time.second* 10**6 + current_time.microsecond
             if not self.inputcursorblinking or ((microseconds//1000) // self.inputcursorblinkspeed) % 2:
